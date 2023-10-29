90a309c1d299a234635c598262c66f8001c79feb
Ramy Tanios <ramy.tanios@gmail.com>
2023-10-29 10:03:12 +0100

    init wip

---
+++ main.scala
@@ -0,0 +1,8 @@
+package main
+
+object Main
+    extends CommandIOApp(
+      name = "live server",
+      header = "Purely functional live server with hot reload functionality",
+      version = "0.0.1"
+    ) {}
